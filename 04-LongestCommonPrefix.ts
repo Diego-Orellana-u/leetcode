660d7430ef27627490791373d05f4fb3deae5861
diego orellana <diego.orellana.git@gmail.com>
2025-04-14 22:03:12 -0400

    added new leetcode exercise solution

--- 04-LongestCommonPrefix.ts
+++ 04-LongestCommonPrefix.ts
@@ -34,3 +34,18 @@ function longestCommonPrefix(strs: string[]): string {
   }
   return "";
 }
+
+// A better solution made by our lord chatgpt:
+function longestCommonPrefixTwo(strs: string[]): string {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1);
+      if (prefix === "") return "";
+    }
+  }
+
+  return prefix;
+}
