b8386d880f163f9b3269a7b53c1a6b75822ed2eb
diego orellana <diego.orellana.git@gmail.com>
2025-05-01 06:25:03 -0400

    modified new c exercise

--- c-exercises/temperature.c
+++ c-exercises/temperature.c
@@ -12,7 +12,7 @@ main()
   fahr = lower;
   while (fahr <= upper){
     celsius = 5 * (fahr-32) / 9;
-    printf("%3d\t%d\n", fahr, celsius);
+    printf("%3d%6d\n", fahr, celsius);
     fahr = fahr + step;
   }
 }
