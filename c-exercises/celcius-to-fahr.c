44b1b22c345a131ebb638eb13fce568f9dad7aa6
diego orellana <diego.orellana.git@gmail.com>
2025-05-07 07:34:24 -0400

    modified new c exercise

---
+++ c-exercises/celcius-to-fahr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+{
+  float celcius, fahr;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 180;
+  step = 20;
+
+  while(celcius <= upper){
+    fahr = (celcius * 9.0/5.0) + 32;
+    printf("%3f %6f \n", celcius, fahr);
+    celcius = celcius + step;
+  }
+
+}
